56c1f34850135c8d764180ffbe6be2266b6f4f2c
Judith2-web <ighodarojudith91@gmail.com>
2025-06-21 23:38:18 +0100

    Working on variables

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ console.log(`I like pizza!`);
 window.alert(`This is an alert`);
 window.alert(`I like Pizza!`);
 
-document.getElementById("myH1").textContent = `Hello`:
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `My first JavaScript`;
+
+//variable = A container that stores a value.
+//           Behaves as if it were the value it contains.
+
+// 1. declaration  let x;
+// 2. assignment   x = 100;
+
+let x;
+x = 123;
+
+console.log(x);
+
+Let age = 25;
+let price = 10.99;
+
+console.log(`You are $(age) years old`);
+console.log(`The price is $(Price)`);
+
 
 //this is a comment
