a818baf4d8c848f3e9b3514f62f3dfaa9fc64c34
Judith2-web <ighodarojudith91@gmail.com>
2025-06-24 18:53:33 +0100

    JavaScript Variables

--- index.js
+++ index.js
@@ -20,9 +20,22 @@ console.log(x);
 
 let age = 25;
 let price = 10.99;
+let gpa = 2.1;
+
+let firstName = "Jay";
+let favouriteFood = "Yam & Eggs";
+let email = "Jay123@gmail.com";
+
+
+console.log(typeof firstName);
+console.log(`Your name is ${firstName}`);
+console.log(`You like ${favouriteFood}`);
+console.log(`Your email is ${email}`);
+
 
 console.log(`You are ${age} years old`);
-console.log(`The price is ${price}`);
+console.log(`The price is $${price}`);
+console.log(`Your gpa is: ${gpa}`);
 
 
 //this is a comment
